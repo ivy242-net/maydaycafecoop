d27f2c207bab10ef2b6abcd5b875d69730d8c367
Ilona Brand <theilonabrand@gmail.com>
2025-03-29 12:56:16 -0500

    Get the library gallery working. Still needs pagination if there are lots of images, and also I was playing around with a save image button but couldn't yet get it working. i think it would be more useful to have a Create Post From Image button or something that routes to the post page with the image already there. probably all of this is overkill unless mira asks for it though and it'd be more useful to get the media picker to allow you to select from the library

--- client/src/components/LibraryGallery.jsx
+++ client/src/components/LibraryGallery.jsx
@@ -1,4 +1,6 @@
 import { h } from 'preact';
+// uninstall if i decide not to use
+// import { Buffer } from 'buffer';
 import { useState, useEffect } from 'preact/hooks';
 
 function LibraryGallery({ label }) {
@@ -6,7 +8,16 @@ function LibraryGallery({ label }) {
 
     useEffect(() => {
       const getData = async () => {
-        const data = await pb.collection('specials').getFullList(200)
+        let data = await pb.collection('specials').getFullList(200)
+        data.forEach(async (record) => {
+            record['imageUrl'] = pb.files.getURL(record, record['media']);
+
+            // Not sure if this is worth doing
+            // const response = await fetch(record['imageUrl']);
+            // const contentType = response.headers.get('Content-Type');
+            // const buffer = await response.arrayBuffer();
+            // record['imageDataUrl'] = 'data:' + contentType + ';base64,' + Buffer.from(buffer).toString('base64');
+        })
         updateData(data);
       }
       getData();
@@ -14,9 +25,21 @@ function LibraryGallery({ label }) {
   
     return data && (
         <>
-            {data.map((special) => <li>
-                {special['content']}
-            </li>)}
+            {data.map((special) => 
+            <div class="card bg-base-100 w-96 mt-5 shadow-sm">
+            <figure>
+              <img
+                src={special['imageUrl']}
+                alt={special['content']} />
+            </figure>
+            <div class="card-body">
+              <p>{special['content']}</p>
+              <div class="card-actions justify-end">
+                {/* <a href="#" class="btn btn-primary" id="btn-download" download={special['imageUrl']}>Save Image</a> */}
+              </div>
+            </div>
+          </div>
+            )}
         </>
     )
 }
