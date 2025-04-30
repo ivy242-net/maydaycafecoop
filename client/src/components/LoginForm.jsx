fecdd85ec687f83f48a1751c6421489e982d4bfc
Henry Holtgeerts <henryholtgeerts@gmail.com>
2025-04-30 12:30:56 -0500

    feature: add email input to login

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'preact/hooks';
 
 function LoginForm({ label }) {
 
+    const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
 
@@ -10,7 +11,7 @@ function LoginForm({ label }) {
         e.preventDefault();
         try {
             const authData = await pb.collection('users').authWithPassword(
-                'editor@ivy242.net',
+                email,
                 password,
             );
             window.location.href = '/post';
@@ -24,6 +25,8 @@ function LoginForm({ label }) {
             {error && <div>{error}</div>}
             <form onSubmit={(e) => handleSubmit(e)}>
                 <fieldset class="fieldset">
+                    <label class="fieldset-label">Email</label>
+                    <input type="email" class="input w-full" placeholder="Email" onChange={(e) => setEmail(e.target.value)} value={email} />
                     <label class="fieldset-label">Password</label>
                     <input type="password" class="input w-full" placeholder="Password" onChange={(e) => setPassword(e.target.value)} value={password} />
                     <button type="submit" class="btn btn-neutral mt-4">Submit</button>
